f051ff9e66a204b0934c42e09abeb3721fa43ff2
PeterHu <hwj8668250@163.com>
2020-02-21 01:58:06 +0800

    添加跳帧代码（利用vs断线bug）

--- example/advance_frame_usingVSbug.py
+++ example/advance_frame_usingVSbug.py
@@ -1,7 +1,7 @@
 from NSController import Controller
 ctr = Controller()
 #先触发vs断网跳帧的bug，然后进入设置-->日期与时间，光标停留在日期和时间上
-for i in range(1,7558):#示例：跳至7558帧
+for i in range(1,7350):#示例：跳至7558帧
     if(i%200 == 0):#每跳200帧存档1次
         ctr.h()
         ctr.pause(2)
@@ -43,6 +43,35 @@ for i in range(1,7558):#示例：跳至7558帧
         ctr.A()
         ctr.d()
         ctr.d()
+        ctr.d()
+        # year+1
+        ctr.A()
+        ctr.l()
+        ctr.l()
+        ctr.l()
+        ctr.l()
+        ctr.l()
+        ctr.u()
+        ctr.r()
+        ctr.r()
+        ctr.r()
+        ctr.r()
+        ctr.r()
+        ctr.A()
+        # year-1
+        ctr.A()
+        ctr.l()
+        ctr.l()
+        ctr.l()
+        ctr.l()
+        ctr.l()
+        ctr.d()
+        ctr.r()
+        ctr.r()
+        ctr.r()
+        ctr.r()
+        ctr.r()
+        ctr.A()
     else:
         #year+1
         ctr.A()
